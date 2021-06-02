2b88af9a594d0c2429ee7096f433b9a1a919882b
Luiz Moraes <lffm1994@gmail.com>
2021-06-02 13:18:18 -0300

    Módulo 1 - Pacotes e imports padrões

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,7 @@
+import modelo.Cliente
+import novomodelo.Cliente as NovoCliente
+//import java.lang.String as StringJava
+
 fun main() {
 //    println("Bem vindo ao Bytebank")
 //    testaComportamentosConta()
@@ -7,7 +11,15 @@ fun main() {
 //    testaFuncionarios()
     testaAutenticacao()
 
+    val palavra: kotlin.String = ""
+    //Mesma coisa com uso do alias
+//    val palavraJava: StringJava = StringJava("")
+//    val palavraJava: java.lang.String = java.lang.String("")
 
+
+    val cliente = Cliente(nome = "Alex", cpf = "", senha = 1234)
+    //tecnica de utilizar apelidos no import para classes com mesmo nome para evitar conflito
+    val clienteNovo = NovoCliente()
 
 }
 
